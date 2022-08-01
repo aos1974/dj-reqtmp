4a9e05888734106c26b702230c11ad261b904635
Aleksandr Sysoev <aosysoev@narod.ru>
2022-08-01 17:01:39 +0300

    load_stations

--- pagination/stations/views.py
+++ pagination/stations/views.py
@@ -1,9 +1,23 @@
+import os, csv
+from pagination.settings import BUS_STATION_CSV
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+BUSSTATIONS_LIST = []
 
 def index(request):
+    if len(BUSSTATIONS_LIST) == 0:
+        load_stations(BUS_STATION_CSV)
     return redirect(reverse('bus_stations'))
+
+# функция загрузки списка остановок
+def load_stations(fname: str):
+    if os.path.isfile(fname):
+        with open(fname, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for raw in reader:
+                BUSSTATIONS_LIST.append(raw)
+# end load_stations()
 
 
 def bus_stations(request):
@@ -15,3 +29,4 @@ def bus_stations(request):
     #     'page': ...,
     }
     return render(request, 'stations/index.html', context)
+ 
